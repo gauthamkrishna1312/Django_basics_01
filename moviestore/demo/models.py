c933b3efa957223bc0acba9da8dcffb7aa247d46
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2021-08-03 21:13:13 +0530

    second commit

--- moviestore/demo/models.py
+++ moviestore/demo/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 # Create your models here.
+
 class Book(models.Model):
-    tittle = models.CharField(max_length=50)
+    tittle = models.CharField(max_length=50, blank=True, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.DecimalField(decimal_places=5, max_digits=10, blank=False)
+    docs = models.FileField(blank=True)
